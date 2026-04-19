2ccfc0ecf0d9df8dcdf35dbbaf16d124243ef531
Daniel Valle <danielvalleortiz@gmail.com>
2026-04-19 00:40:27 -0500

    guard/services/environments-Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3LJQ_SfoTk5vzpWhvro8psiHnoeNL1SY",
+    authDomain: "mydigitalwallet-d46ef.firebaseapp.com",
+    projectId: "mydigitalwallet-d46ef",
+    storageBucket: "mydigitalwallet-d46ef.firebasestorage.app",
+    messagingSenderId: "3239260508",
+    appId: "1:3239260508:web:139a7077cb9bfdb6ec9199",
+    measurementId: "G-BR3ZVXPBMY"
+  }
 };
